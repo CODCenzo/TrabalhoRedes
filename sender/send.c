d008646a29fb4424c9a9ad6c093ff2147805eed8
AleSbrissia <alexandresbrissia@ufpr.br>
2026-04-28 16:05:37 -0300

    comecando a mandar aquivos maiores

--- sender/send.c
+++ sender/send.c
@@ -13,6 +13,7 @@ uint8_t gera_byte_aleat (uint8_t min, uint8_t max) {
 }
 
 // Constrói e preenche o frame.
+// nao aloca o buffer
 unsigned char* build_kermit(unsigned char *buffer_dados, uint8_t tamMsg, uint8_t seq,
                  uint8_t type, uint8_t crc) {
 
